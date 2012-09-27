9add9810412a330ad22ad1319fc246cfa2a5c331
errt <dominik.helm@stud.tu-darmstadt.de>
2012-09-27 17:09:14 +0200

    Add JsonReader.convertString()

--- src/main/scala/ftanml/util/JsonReader.scala
+++ src/main/scala/ftanml/util/JsonReader.scala
@@ -3,6 +3,15 @@ package ftanml.util
 import java.io.Reader
 import java.util.Stack
 import java.text.ParseException
+import java.io.StringReader
+
+object JsonReader {
+	def convertString(string: String) : String = {
+		val char: Array[Char] = new Array[Char](string.length)
+		new JsonReader(new StringReader(string)).read(char)
+		String.valueOf(char).trim()
+	}
+}
 
 class JsonReader(base: Reader) extends Reader {
 	private var depthMapsArray = new Stack[Int] //0: Map, 1: Array (tried to do it with an enum but couldn't get that working
@@ -18,12 +27,12 @@ class JsonReader(base: Reader) extends Reader {
 			case '\\' => if(inString) escaped = !escaped; '\\'
 			case _ if(inString) => char
 			case '{' => depthMapsArray.push(0); '<'
-			case '}' => if(depthMapsArray.pop == 0) throw new ParseException("Expected ], found }", offset-1); '>'
+			case '}' => if(depthMapsArray.pop != 0) throw new ParseException("Expected ], found }", offset-1); '>'
 			case '[' => depthMapsArray.push(1); '['
-			case ']' => if(depthMapsArray.pop == 1) throw new ParseException("Expected }, found ]", offset-1); ']'
+			case ']' => if(depthMapsArray.pop != 1) throw new ParseException("Expected }, found ]", offset-1); ']'
 			case ':' => '='
 			case ',' if(depthMapsArray.peek == 0) => read
-			case -1 => if(depthMapsArray.size == 0) throw new ParseException("End of file reached, but map or array unclosed", offset-1); -1
+			case -1 => if(depthMapsArray.size != 0) throw new ParseException("End of file reached, but map or array unclosed", offset-1); -1
 			case _ => char
 		}
 	}
--- src/test/scala/JsonReaderTest.scala
+++ src/test/scala/JsonReaderTest.scala
@@ -4,34 +4,27 @@ import org.scalatest.FlatSpec
 import java.io.StringReader
 import ftanml.util.JsonReader
 import org.scalatest.matchers.ShouldMatchers
+import java.text.ParseException
 
 class JsonReaderTest extends FlatSpec with ShouldMatchers{
 	
 	"A JsonReader" must "be able to convert valid JSON to valid FtanML" in {
-		convertString("true") should equal ("true")
-		convertString("[1, 2.3]") should equal ("[1, 2.3]")
-		convertString("{\"att1\":\"val1\", \"att2\":\"val2\"}") should equal ("<\"att1\"=\"val1\" \"att2\"=\"val2\">")
+		JsonReader.convertString("true") should equal ("true")
+		JsonReader.convertString("[1, 2.3]") should equal ("[1, 2.3]")
+		JsonReader.convertString("{\"att1\":\"val1\", \"att2\":\"val2\"}") should equal ("<\"att1\"=\"val1\" \"att2\"=\"val2\">")
 	}
 	
 	it should "fail on obviously invalid JSON" in {
-		intercept[AssertionError]{
-			convertString("[")
+		intercept[ParseException]{
+			JsonReader.convertString("[")
 		}
-		intercept[AssertionError]{
-			convertString("{]")
+		intercept[ParseException]{
+			JsonReader.convertString("{]")
 		}
 	}
 	
 	it should "be able to convert even more complex JSON to valid FtanML" in {
-		convertString("{\"att1\":[true, false], \"att2\":15.2e5}") should equal ("<\"att1\"=[true, false] \"att2\"=15.2e5>")
-		convertString("[null, {\"att1\":null, \"att2\":[10]}, false]") should equal ("[null, <\"att1\"=null \"att2\"=[10]>, false]")
+		JsonReader.convertString("{\"att1\":[true, false], \"att2\":15.2e5}") should equal ("<\"att1\"=[true, false] \"att2\"=15.2e5>")
+		JsonReader.convertString("[null, {\"att1\":null, \"att2\":[10]}, false]") should equal ("[null, <\"att1\"=null \"att2\"=[10]>, false]")
 	}
-	
-	def convertString(string: String): String = {
-		val reader: JsonReader = new JsonReader(new StringReader(string))
-		val char: Array[Char] = new Array[Char](string.length)
-		reader.read(char)
-		String.valueOf(char).trim()
-	}
-
 }
