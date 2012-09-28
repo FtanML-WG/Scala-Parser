27f9e7bc23248fe356bb995b4bd05c1537631a5b
smessmer <mail@smessmer.de>
2012-09-28 02:19:58 +0200

    AllOfType/AnyOfType applicaple in broader context (Traversable instead of Seq) + easier functional implementation

--- src/main/scala/ftanml/types/AnyOfType.scala
+++ src/main/scala/ftanml/types/AnyOfType.scala
@@ -6,11 +6,6 @@ import ftanml.objects.FtanValue
  * A type that matches the union of several supplied types
  */
 
-class AnyOfType (memberTypes : Seq[FtanType]) extends FtanType {
-  def matches(value: FtanValue) : Boolean = {
-    for (f <- memberTypes) {
-      if (f.matches(value)) return true
-    }
-    false
-  }
+class AnyOfType (memberTypes : Traversable[FtanType]) extends FtanType {
+  def matches(value: FtanValue) : Boolean = memberTypes.exists{_.matches(value)}
 }
