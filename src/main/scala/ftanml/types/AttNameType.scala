36decda0e8553bcfeef2bc977808c08f65bacc32
Michael Kay <mike@saxonica.com>
2012-10-09 10:44:26 +0100

    Fix tests broken because serialization format for numbers changed
    We now serialize as "123" instead of "123.0" which unfortunately
    changes the results of parser tests.

---
+++ src/main/scala/ftanml/types/AttNameType.scala
@@ -0,0 +1,20 @@
+package ftanml.types
+
+import ftanml.objects.{FtanValue, FtanString, FtanElement}
+
+/**
+ * A NameType is a facet applied to elements, that constrains the name of the
+ * element to have a particular type. If the type is nullable, then the element
+ * name is optional, but if present it must conform to the type given. Common uses
+ * are to constrain the name to a particular string (denoted by a FixedValueTest),
+ * an enumeration of alternatives, or a regular expression.
+ */
+
+class AttNameType(theType : FtanType) extends FtanType {
+  def matches(value: FtanValue) = {
+    value match {
+      case v: FtanElement => v(FtanElement.NAME_KEY).isInstance(theType)
+      case _ => false
+    }
+  }
+}
